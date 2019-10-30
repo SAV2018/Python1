3b342ba53154d524e97ce157c7daaa87b924d21c
Sav <arconar@mail.ru>
2019-10-30 05:56:31 +0300

    Lesson 2 - 6

---
+++ task6.py
@@ -0,0 +1,67 @@
+'''
+Реализовать структуру данных «Товары». Она должна представлять собой список кортежей.
+Каждый кортеж хранит информацию об отдельном товаре. В кортеже должно быть два элемента — номер товара и словарь
+с параметрами (характеристиками товара: название, цена, количество, единица измерения).
+Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
+Пример готовой структуры:
+[
+    (1, {“название”: “компьютер”, “цена”: 20000, “количество”: 5, “eд”: “шт.”}),
+    (2, {“название”: “принтер”, “цена”: 6000, “количество”: 2, “eд”: “шт.”}),
+    (3, {“название”: “сканер”, “цена”: 2000, “количество”: 7, “eд”: “шт.”})
+]
+Необходимо собрать аналитику о товарах. Реализовать словарь, в котором каждый ключ — характеристика товара,
+например название, а значение — список значений-характеристик, например список названий товаров.
+Пример:
+{
+    “название”: [“компьютер”, “принтер”, “сканер”],
+    “цена”: [20000, 6000, 2000],
+    “количество”: [5, 2, 7],
+    “ед”: [“шт.”]
+}
+'''
+
+enter = ''
+goods = []
+i = 0
+
+while enter == '': # если нажата клавиша Enter - вводим данные, иначе выходим
+    i += 1
+
+    name = input('\nEnter name of good: ')
+    price = input('Enter price: ')
+    num = input('Enter quantity of good: ')
+    unit = input('Enter unit: ')
+
+    goods.append((i, {'name': name, 'price': price, 'num': num, 'unit': unit}))
+    print('\n', goods)
+
+    enter = input('\nPress Enter for continue, any key+Enter to exit...')
+
+# вывод "аналитики"
+while True:
+    print('\nChoose action: ')
+    print(' [1] Print list of goods.')
+    print(' [2] Print list of prices.')
+    print(' [3] Print quantities.')
+    print(' [4] Print units.')
+    print(' [5] Exit.')
+
+    action = input('\nYour choice: ')
+    if action == '5':
+        break
+
+    names = ('Goods', 'Prices', 'Quantities', 'Units')
+    titles = ('name', 'price', 'num', 'unit')
+    res = {'name': [], 'price': [], 'num': [], 'unit': set()}
+
+    for id, v in goods:
+        res['name'].append(v['name'])
+        res['price'].append(v['price'])
+        res['num'].append(v['num'])
+        res['unit'].add(v['unit'])
+
+    print(res)
+
+    print(f'\n{names[int(action)-1]}: {res[titles[int(action)-1]]}')
+
+print('\nGoodbye!')
